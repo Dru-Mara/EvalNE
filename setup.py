d0f70675c4a784e60354947cbbe4ab28bedbd2ed
Dru Mara <dru3004@gmail.com>
2020-11-30 12:51:20 +0100

    EvalNE v0.3.2 fix2

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+"""
+Setup script for EvalNE. You can install the library globally using:
+
+python setup.py install
+
+Or for a single user with:
+
+python setup.py install --user
+"""
+
 from setuptools import setup, find_packages
 
 setup(
@@ -10,25 +20,30 @@ setup(
     description="Open Source Network Embedding Evaluation toolkit",
     long_description=open("./README.md").read(),
     long_description_content_type="text/markdown",
-    keywords='evaluation embedding link-prediction networks graphs',
+    keywords='evaluation embedding link-prediction sign-prediction node-classification network-reconstruction '
+             'networks graphs visualization',
     packages=find_packages(),
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
+    python_requires='>=2.6, <=3.6',
     zip_safe=False,
+    tests_require=["pytest", "pytest-cov"],
     install_requires=[
-        'numpy==1.15.1',
+        'numpy==1.15.4',
         'scikit-learn==0.19.0',
         'networkx==2.2',
         'scipy==0.19.1',
         'matplotlib==2.2.4',
-        'tqdm',
-        'pandas'
+        'pandas==0.24.2',
+        'tqdm'
     ],
     classifiers=[
+        "Intended Audience :: Science/Research"
+        "Intended Audience :: Developers"
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Intended Audience :: Science/Research"
+        "Operating System :: OS Independent",
     ]
 )
